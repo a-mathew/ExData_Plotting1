e09dcb55d0f505efbd4b4c288269fe596966e3b8
anumathew <anumathews493@gmail.com>
2017-10-14 01:23:47 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,4 @@
-if(!exists("datas"))
-  { source("load_data.R")} ## Loading dataset only if it is not preexisting in the global environment
-
+source("load_data.R")} ## Loading dataset
 hist(datas$Global_active_power, col = "red",main = "Global Active Power",xlab = "Global Active Power (killowatts)")
 
 ##Copying to png file
